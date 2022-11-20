75a296f9a2cddd919fc9d69bc870331b38f8a849
Ziz Coding DE <117445501+DotVisual@users.noreply.github.com>
2022-11-20 13:48:24 +0700

    Create ziz-simi.py

---
+++ ziz-simi.py
@@ -0,0 +1,115 @@
+import os,sys,requests,shlex,random,re
+from datetime import datetime
+from datetime import date
+from requests import get
+ZizCodingDE = " ".join(map(shlex.quote, sys.argv[1:]))
+#gboleh = "D" and "R" and "K" and "G" and "A" and "Y","I","4"
+#word = str(ZizCodingDE)
+if re.findall('^.*[dD].*[rR].*[kK].*[gG].*[3eEaA4].*[yYi1I]', mr_dark):
+	print(f'_simi bot_: {random.choice(["ndas mu gay","lu yg gay anj","gay teriak gay","lu gay asw"])}')
+	print ('Coded By Ziz Coding DE')
+elif '628999109535' in Ziz Coding DE:
+	print(f'_simi bot_: {random.choice(["itu owner aku bang jangan di tag >_<","dia owner aku bang, tolong jangan di tag :)","ada perlu apa sama owner saya? >-<"])}')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'dark':
+	print(f'_simi bot_: {random.choice(["ganteng","pro","owner saya tuh","owner saya kak","hengker slebew","kenapa panggil owner saya?"])}')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'wira':
+	print('_simi bot_: wibu')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'help':
+	print('_simi bot_: apaan anying, tinggal make ae g bisa, wuuuu cupuu')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'kiw':
+	print('_simi bot_: kiw itu bocil yang suka nonton bokev')
+	print('Coded By Ziz Coding De')
+elif ZizCodingDE == 'owner':
+	print('_simi bot_: DotVisual')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'aldo':
+	print(f'_simi bot_: {random.choice(["hengker epep dia tuh","hati2 dia bisa hek bumi","dia hengker cuy hati2","ngeri hengker epep","esempe"])}')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'bayu':
+	print(f'_simi bot_: {random.choice(["Hengker bash","polygon","pejuang kentang","...","yt dia https://youtube.com/channel/UCtu-GcxKL8kJBXpR1wfMgWg"])}')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'silent':
+	print('_simi bot_: knp ko disuruh diem bg')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'sofwan':
+	print('_simi bot_: kang ngebot discord')
+	print('Coded By Ziz Coding DE')
+elif ZizCodingDE == 'aushine':
+	print('_simi bot_: orang random yang suka read pesan')
+	print('Coded By Ziz Coding DE')
+elif 'ireng' in Ziz Coding DE:
+	print(f'_simi bot_: {random.choice(["@PickFord dia ireng ygy","Lu ireng","dasar ireng","ireng teriak ireng","ngaca deck","lu yg ireng asw","dasar ireng g tau diri"])}')
+	print('Coded By Ziz Coding DE')
+elif 'gay' in Ziz Coding DE:
+	print('_simi bot_: @Uby dia ril gay cuy')
+	print('Coded By Ziz Coding DE')
+elif '6285715873604' in Ziz Coding DE:
+	print(f'_simi bot_: {random.choice(["dia orang gay kata dark","ireng","hati2 dia gay kak!","Jangan deket2 sama orang gay!"])}')
+	print('Coded By Ziz Coding DE')
+elif 'mau' in Ziz Coding DE:
+	print('_simi bot_: g mau')
+	print('Coded By Ziz Coding DE')
+elif 'jam b' in Ziz Coding DE:
+	sekarang = datetime.now()
+	print(f'_simi bot_: {sekarang.strftime("%H:%M:%S")}')
+	print('Ziz Coding DE')
+elif 'pacaran' in Ziz Coding DE:
+	print('_simi bot_: astagfirullah haram brother')
+	print('Coded By Ziz Coding DE')
+elif 'ammar' and 'Ammar' in Ziz Coding DE:
+	print('_simi bot_: ammar? esempeh')
+	print('Coded By Ziz Coding DE')
+elif 'tanggal' in Ziz Coding DE:
+	print(f'_simi bot_: {date.today()}')
+	print('Coded By Ziz Coding DE')
+elif '62882296' in Ziz Coding DE:
+	print(f'_simi bot_: {random.choice(["ammar executed","dia wibu kak","dia masih jomblo kata dark","njer dia masih esempeh"])}')
+	print('Coded By Ziz Coding DE')
+#elif '628999109535' in Ziz Coding DE:
+#	print(f'_simi bot_: {random.choice(["itu owner aku bang jangan di tag >_<","dia owner aku bang, tolong jangan di tag :)","ada perlu apa sama owner saya? >-<"])}')
+#	print('Coded By Ziz Coding DE')
+#elif 'dark g4y' and 'dark gei' and 'dark ge1' and 'dark ga1' and 'dark gey' and 'd4rk gay' in mr_dark:
+#	print(f'_simi bot_: {random.choice(["ndas mu gay","lu yg gay anj","gay teriak gay","lu gay asw"])}')
+#	print('Coded By Ziz Coding DE')
+else:
+	#mr_ziz_url = f'https://api.simsimi.net/v2/?text={mr_ziz}&lc=id'
+	#dark = requests.get(mr_ziz_url)
+	cookies = {
+	    '_ga': 'GA1.1.666863305.1650578072',
+	    '_ga_L8BJYNG33X': 'GS1.1.1650578072.1.0.1650578072.0',
+	    'PHPSESSID': 'a0785e719917ca2355a1260ca60746c9',
+	}
+
+	headers = {
+	    'authority': 'simsimi.info',
+	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+	    'accept-language': 'id,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
+	    'cache-control': 'max-age=0',
+	    # Requests sorts cookies= alphabetically
+	    # 'cookie': '_ga=GA1.1.666863305.1650578072; _ga_L8BJYNG33X=GS1.1.1650578072.1.0.1650578072.0; PHPSESSID=a0785e719917ca2355a1260ca60746c9',
+	    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Microsoft Edge";v="100"',
+	    'sec-ch-ua-mobile': '?0',
+	    'sec-ch-ua-platform': '"Windows"',
+	    'sec-fetch-dest': 'document',
+	    'sec-fetch-mode': 'navigate',
+	    'sec-fetch-site': 'none',
+	    'sec-fetch-user': '?1',
+	    'upgrade-insecure-requests': '1',
+	    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.44',
+	}
+
+	params = {
+	    'text': mr_ziz,
+	    'lc': 'id',
+	}
+
+	dark = requests.get('https://simsimi.info/api/', headers=headers, params=params, cookies=cookies).text
+	if 'success' in dark:
+	     print('_simi bot_: ', dark.split('"message": "')[1].split('"')[0])
+	     print('Coded By Ziz Coding DE')
+	else:
+	     print('bad respon ')
