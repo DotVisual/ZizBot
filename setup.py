bb07c5d0fe8c9fa5fa7ff43ee6d8522ccb48f4e6
Ziz Coding DE <117445501+DotVisual@users.noreply.github.com>
2022-11-20 12:15:53 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+import re
+from setuptools import setup, find_packages
+INSTALL_REQUIRES = ['requests',
+                    'phonenumbers',
+                    'instaloader',
+                    'pycoingecko'
+                    ]
+setup(
+install_requires=INSTALL_REQUI
